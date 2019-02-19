3da03a9795e78a5f7f68b943294aeaf4715ad37e
laxezice <61070048@kmitl.ac.th>
2019-02-19 22:53:16 +0700

    new update

--- js/board.js
+++ js/board.js
@@ -1,23 +1,67 @@
+function gameStart(num){
+    turn = "player1";
+    player = {
+        player1:{
+            step: 1,
+            life: 3,
+            status: 1},
+        player2:{
+            step: 1,
+            life: 3,
+            status: 1},
+        player3:{
+            step: 1,
+            life: 3,
+            status: 0},
+        player4:{
+            step: 1,
+            life: 3,
+            status: 0},
+    };
+    if (num.innerHTML === "3 Player"){
+       player.player3.status = 1;
+       document.getElementById("player3").style.display = 'block';
+    } else if (num.innerText === "4 Player"){
+        player.player3.status = 1;
+        document.getElementById("player3").style.display = 'block';
+        player.player4.status = 1;
+        document.getElementById("player4").style.display = 'block';
+    }
+    document.getElementById("selectPlayer").style.display = 'none';
+    document.getElementById("game").style.display = 'block';
+    theDice = document.getElementsByTagName("dice");
+    for (i = 0; i<theDice.length; i++){
+        theDice[i].style.display = 'inline-block'
+    }
+    console.log(player)
+    turnStart()
+}
+
+
 function turnStart(){
-    theTurn = document.getElementById("theTurn");
-    player = theTurn.getAttribute("turn");
-    theTurn.innerText = player;
-    thePlayer = thePlayer = document.getElementById(player);
+    document.getElementById("theTurn").innerText = turn.toLocaleUpperCase()+"\'s Turn";
+    thePlayer = document.getElementById(turn);
     // console.log(thePlayer);
     thePlayer.setAttribute("animate", "1");
 }
 
 function turnEnd(){
-    console.log("end1");
-    if(player === "player1"){
+    if(turn === "player1"){
         thePlayer.setAttribute("animate", "0");
-        theTurn.setAttribute("turn", "player2");
+        turn = "player2";
+        turnStart();
+    } else if (turn === "player2" && player.player3.status === 1) {
+        thePlayer.setAttribute("animate", "0");
+        turn = "player3";
+        turnStart();
+    }  else if (turn === "player3" && player.player4.status === 1) {
+        thePlayer.setAttribute("animate", "0");
+        turn = "player4";
         turnStart();
     } else {
         thePlayer.setAttribute("animate", "0");
-        theTurn.setAttribute("turn", "player1");
+        turn = "player1";
         turnStart();
-        console.log(theTurn);
     }
 }
 
@@ -37,22 +81,21 @@ function diceFunction() {
 }
 
 function moveCounter(move) {
-    start = +(thePlayer.getAttribute("step"));
+    start = player[turn].step;
     end = start+move;
     moveFunction(start, end)
 }
 
 function moveFunction(start, end) {
-    start += 1;
+    start++;
     setTimeout(function(){
         // console.log(start);
         thePlayer.setAttribute("step", start);
         if(start < end){
             moveFunction(start, end);
         } else {
+            player[turn].step = end;
             turnEnd();
         }}, 400);
 
 }
-
-turnStart()
