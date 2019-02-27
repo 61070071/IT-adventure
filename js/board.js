4104ed6c3cdbf38264165e12f99544a3fe462865
laxezice <61070048@kmitl.ac.th>
2019-02-27 18:24:36 +0700

    up

--- js/board.js
+++ js/board.js
@@ -2,37 +2,35 @@ function gameStart(num){
     turn = "player1";
     player = {
         player1:{
-            step: 1,
+            step: 0,
             life: 3,
             status: 1},
         player2:{
-            step: 1,
+            step: 0,
             life: 3,
             status: 1},
         player3:{
-            step: 1,
+            step: 0,
             life: 3,
             status: 0},
         player4:{
-            step: 1,
+            step: 0,
             life: 3,
             status: 0},
     };
     if (num.innerHTML === "3 Player"){
-       player.player3.status = 1;
-       document.getElementById("player3").style.display = 'block';
+    player.player3.status = 1;
+       document.getElementById("player3").style.display = 'inline-block';
     } else if (num.innerText === "4 Player"){
         player.player3.status = 1;
-        document.getElementById("player3").style.display = 'block';
+        document.getElementById("player3").style.display = 'inline-block';
         player.player4.status = 1;
-        document.getElementById("player4").style.display = 'block';
+        document.getElementById("player4").style.display = 'inline-block';
     }
     document.getElementById("selectPlayer").style.display = 'none';
-    document.getElementById("game").style.display = 'block';
-    theDice = document.getElementsByTagName("dice");
-    for (i = 0; i<theDice.length; i++){
-        theDice[i].style.display = 'inline-block'
-    }
+    document.getElementById("theBoard").style.display = 'inline-block';
+    document.getElementById("theTurn").style.display = 'inline-block';
+    document.getElementById("theDice").style.display = 'inline-block';
     console.log(player);
     b1 = document.getElementById('b1');
     turnStart()
@@ -67,18 +65,19 @@ function turnEnd(){
 }
 
 function diceFunction() {
-    dice1 = Math.floor(Math.random() * 6) + 1;
-    dice2 = Math.floor(Math.random() * 6) + 1;
-    theDice1 = document.getElementById("theDice1");
-    theDice2 = document.getElementById("theDice2");
-    theDice1.setAttribute("score", "0");
-    theDice2.setAttribute("score", "0");
-    setTimeout(function () {
-        theDice1.setAttribute("score", dice1);
-        theDice2.setAttribute("score", dice2);
-        moveCounter(dice1+dice2)
-    }, 1500);
-
+    if (fighting === 0){
+        dice1 = Math.floor(Math.random() * 6) + 1;
+        dice2 = Math.floor(Math.random() * 6) + 1;
+        theDice1 = document.getElementById("theDice1");
+        theDice2 = document.getElementById("theDice2");
+        theDice1.setAttribute("score", "0");
+        theDice2.setAttribute("score", "0");
+        setTimeout(function () {
+            theDice1.setAttribute("score", dice1);
+            theDice2.setAttribute("score", dice2);
+            moveCounter(dice1+dice2)
+        }, 1500);
+    }
 }
 
 function moveCounter(move) {
@@ -97,34 +96,61 @@ function moveFunction(start, end) {
         } else {
             player[turn].step = end;
             for(var p in player){
-                if(player[turn].step === player[p].step && turn !== p){
-                    document.getElementById('battle').style.display = 'inline-block';
-                    b1.innerText = turn;
+                if(player[turn].step === player[p].step && turn !== p && player[turn].step != 1){
                     b2.innerText = p;
-                    b1.setAttribute('battle','1');
-                    b2.setAttribute('battle','1');
+                    fighting = 1
                 }
             }
-            turnEnd();
-        }}, 400);
+            if(fighting == 1){
+                b1.style.width = '0px';
+                b1.style.background = document.getElementById(turn).getAttribute("color");
+                b2.style.width = '0px';
+                b2.style.background = document.getElementById(b2.innerText).getAttribute("color");
+                document.getElementById('battle').style.display = 'inline-block';
+                b1.innerText = turn;
+            } else {
+                turnEnd();
+            }
+        }}, 200);
 
 }
 
 function reverseCounter() {
     var reverse = player[loser].step - 5;
-    console.log(reverse);
+    if(reverse < 1){
+        reverse = 1;
+    }
     moveReverse(reverse);
 }
 
 function moveReverse(reverse) {
-    player[loser].step --;
+    if (player[loser].step != 1){
+        player[loser].step --;
+    }
     theLoser = document.getElementById(loser);
     setTimeout(function () {
-        theLoser.setAttribute("step", player[loser].step)
+        theLoser.setAttribute("step", player[loser].step);
         if(player[loser].step > reverse){
             moveReverse(reverse)
+        } else {
+            for(var p in player){
+                if(player[loser].step === player[p].step && loser !== p && player[turn].step != 1){
+                    b2.innerText = p;
+                    fighting = 1
+                }
+            }
+            if(fighting === 1){
+                b1.style.width = '0px';
+                b1.style.background = document.getElementById(loser).getAttribute("color");
+                b2.style.width = '0px';
+                b1.style.background = document.getElementById(b2.innerText).getAttribute("color");
+                document.getElementById('battle').style.display = 'inline-block';
+                b1.innerText = loser;
+            } else {
+                turnEnd();
+            }
         }
-    }, 400);
+    }, 200);
 }
 
 
@@ -132,51 +158,100 @@ var b1 = document.getElementById('b1');
 var b1Bar = 0;
 var b2 = document.getElementById('b2');
 var b2Bar = 0;
+var fighting = 0;
 var loser = '';
-document.onkeydown = document.body.onkeykeypress = function(e)
-{
-    if((e.keyCode == 65 || e.keyCode == 97) && document.getElementById("b1").getAttribute("battle") == "1") {
-        e = e || window.event;
+
+// document.onkeydown = document.body.onkeykeypress = function(e)
+// {
+//     if((e.keyCode == 65 || e.keyCode == 97) && document.getElementById("b1").getAttribute("battle") == "1") {
+//         e = e || window.event;
+//         b1Bar += 8;
+//         if(b1Bar >= 296){
+//             fighting = 0;
+//             b1.style.width = '296px';
+//             win.innerText = b1.innerText + " Win";
+//             loser = b2.innerText;
+//             player[loser].life -= 1;
+//             b1Bar = 0;
+//             b2Bar = 0;
+//             b1.style.width = '0px';
+//             b2.style.width = '0px';
+//             document.getElementById('battle').style.display = 'none';
+//             setTimeout(function () {
+//                 win.style.display = 'none';
+//                 reverseCounter()
+//             }, 1500);
+//
+//         } else {
+//             b1.style.width = b1Bar+'px';
+//         }
+//
+//
+//         // i.innerHTML = 'Hello';
+//         // document.body.style.backgroundColor = "#efeab6"
+//     } else if((e.keyCode == 76 || e.keyCode == 108) && document.getElementById("b2").getAttribute("battle") == "1") {
+//         e = e || window.event;
+//         b2Bar += 8;
+//         if(b2Bar >= 296){
+//             fighting = 0;
+//             b2.style.width = '296px';
+//             win.innerText = b2.innerText + " Win";
+//             loser = b1.innerText;
+//             player[loser].life -= 1;
+//             b1Bar = 0;
+//             b2Bar = 0;
+//             document.getElementById('battle').style.display = 'none';
+//             setTimeout(function () {
+//                 win.style.display = 'none';
+//                 reverseCounter()
+//             }, 1500);
+//         } else {
+//             b2.style.width = b2Bar+'px';
+//         }
+//         // i.innerHTML = 'Hello';
+//         // document.body.style.backgroundColor = "#efeab6"
+//     }
+// };
+
+document.body.onkeyup = function (e) {
+    if (e.code === "KeyA" && fighting == 1){
         b1Bar += 8;
         if(b1Bar >= 296){
+            fighting = 0;
             b1.style.width = '296px';
             win.innerText = b1.innerText + " Win";
             loser = b2.innerText;
-            b1.setAttribute('battle','0');
-            b2.setAttribute('battle','0');
+            player[loser].life -= 1;
             b1Bar = 0;
             b2Bar = 0;
+            b1.style.width = '0px';
+            b2.style.width = '0px';
+            document.getElementById('battle').style.display = 'none';
             setTimeout(function () {
-                document.getElementById('battle').style.display = 'none';
+                win.style.display = 'none';
                 reverseCounter()
             }, 1500);
-
         } else {
             b1.style.width = b1Bar+'px';
         }
-
-
-        // i.innerHTML = 'Hello';
-        // document.body.style.backgroundColor = "#efeab6"
-    } else if((e.keyCode == 76 || e.keyCode == 108) && document.getElementById("b2").getAttribute("battle") == "1") {
-        e = e || window.event;
+    } else if (e.code === "KeyL" && fighting == 1){
         b2Bar += 8;
         if(b2Bar >= 296){
+            fighting = 0;
             b2.style.width = '296px';
             win.innerText = b2.innerText + " Win";
             loser = b1.innerText;
-            b1.setAttribute('battle','1');
-            b2.setAttribute('battle','1');
+            player[loser].life -= 1;
             b1Bar = 0;
             b2Bar = 0;
+            document.getElementById('battle').style.display = 'none';
             setTimeout(function () {
-                document.getElementById('battle').style.display = 'none';
+                win.style.display = 'none';
                 reverseCounter()
             }, 1500);
         } else {
             b2.style.width = b2Bar+'px';
         }
-        // i.innerHTML = 'Hello';
-        // document.body.style.backgroundColor = "#efeab6"
+
     }
 };
